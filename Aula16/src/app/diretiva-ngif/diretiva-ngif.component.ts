021d5fbaef3918d3593d1cdad0c2cb43ecdcb9a4
lbrunol <bruno.ar.avl@gmail.com>
2017-08-21 22:36:47 -0300

    #ngIf

--- Aula16/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ Aula16/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-diretiva-ngif',
-  templateUrl: './diretiva-ngif.component.html',
-  styleUrls: ['./diretiva-ngif.component.css']
+	selector: 'app-diretiva-ngif',
+	templateUrl: './diretiva-ngif.component.html',
+	styleUrls: ['./diretiva-ngif.component.css']
 })
 export class DiretivaNgifComponent implements OnInit {
 
-  constructor() { }
+	cursos: string[] = [];
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+	adicionarCurso() {
+		let inp: HTMLInputElement = <HTMLInputElement> document.querySelector('.ang-txt-curso');
+		this.cursos.push(inp.value);
+		inp.value = '';
+	}
+	  
+	removerCurso(e){
+		if(e.srcElement) {
+			let el: HTMLAnchorElement = <HTMLAnchorElement> e.srcElement;
+			let i: number = parseInt(el.getAttribute('data-index'));
+			this.cursos.splice(i, 1);
+		}
+	}
 
 }
